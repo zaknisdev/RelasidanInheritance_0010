89f167b20b836322b367630fe2935341db6d7cad
zaknisdev <zakianisf1710@gmail.com>
2025-05-26 17:33:09 +0700

    memasukkan data pasien dan dokter ke dalam class

--- asosiasi/asotiation.cpp
+++ asosiasi/asotiation.cpp
@@ -55,3 +55,14 @@ void dokter::cetakPasien() {
     cout << endl;                                   
 }
 
+int main() {
+    dokter* varDokter1 = new dokter("dr.Budi");
+    dokter* varDokter2 = new dokter("dr.Tono");
+    pasien* varPasien1 = new pasien("Andi");
+    pasien* varPasien2 = new pasien("Lia");
+
+    varDokter1->tambahPasien(varPasien1);
+    varDokter1->tambahPasien(varPasien2);
+    varDokter2->tambahPasien(varPasien1);
+}
+
