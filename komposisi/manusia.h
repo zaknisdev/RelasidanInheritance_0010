eab4622cf5de42b62ea70eba74abb472d400b528
zaknisdev <zakianisf1710@gmail.com>
2025-05-26 18:02:09 +0700

    membuat class manusa dengan anggota name varjantung dan konstruktor destruktornya

--- komposisi/manusia.h
+++ komposisi/manusia.h
@@ -2,5 +2,16 @@
 #define MANUSIA_H
 using namespace std;
 
+class manusia {
+    public: 
+    string name;
+    jantung varJantung;
 
+    manusia(string pName) : name(pName) {
+        cout << name << " hidup\n";
+    }
+    ~manusia() {
+        cout << name << " mati\n";
+    }
+};
 #endif
