f73b99c7f87b7e67e60d721b83805bc31ed86709
zaknisdev <zakianisf1710@gmail.com>
2025-05-26 11:37:09 +0700

    membuat fungsi untuk menambah dokter dan mencetak datanya

--- asosiasi/asotiation.cpp
+++ asosiasi/asotiation.cpp
@@ -7,4 +7,13 @@ class pasien { //asosiasi
     public:
         string nama;
         vector<dokter*> daftar_dokter;
-}
+        pasien(string pNama) :nama(pNama) {
+            cout << "pasien \"" << nama << "\" ada\n";
+        }  
+        ~pasien() {
+            cout << "pasien \"" << nama << "\" tidak ada\n";
+        }
+
+        void tambahDokter(dokter*);
+        void cetakDokter();
+};
