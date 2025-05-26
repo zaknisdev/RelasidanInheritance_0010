232ec1609341086969018864912a419a10b6911c
zaknisdev <zakianisf1710@gmail.com>
2025-05-26 11:30:45 +0700

    relasi agregasi ibu akan nantinya dipanggil ke file agregasi.cpp

---
+++ agregasi/ibu.h
@@ -0,0 +1,31 @@
+#ifndef IBU_H
+#define IBU_H
+
+class ibu {
+    public :
+        string nama;
+        vector<anak*> daftar_anak;
+
+        ibu(string pNama) :nama(pNama) {
+            cout << "ibu \"" << nama << "\" ada\n";
+        }
+        ~ibu() {
+            cout << "ibu \"" << nama << "\" tidak ada\n";
+        }
+        void tambahAnak(anak*);
+        void cetakAnak();
+};
+
+void ibu::tambahAnak(anak* pAnak) {
+    daftar_anak.push_back(pAnak);
+}
+
+void ibu::cetakAnak() {
+    cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
+    for (auto& a : daftar_anak)
+ {
+    cout << a->nama << "\n";
+ }
+ cout << endl;
+}
+#endif
