2732d1d55991dbc2bcf56a4fc9c3364e0d20a8ed
zaknisdev <zakianisf1710@gmail.com>
2025-05-26 17:57:45 +0700

    membuat cakss jantung beranggotakan konstruktor dan destruktor

--- komposisi/jantung.h
+++ komposisi/jantung.h
@@ -2,3 +2,14 @@
 #define JANTUNG_H
 using namespace std;
 
+class jantung {
+    public:
+    jantung () {
+        cout << "jantung dihidupkan\n";
+    }
+    ~jantung() {
+        cout << "jantung dimatikan\n";
+    }
+};
+#endif
+
