73a3519f94789e5ab924ec171463d9806ba26922
zaknisdev <zakianisf1710@gmail.com>
2025-05-26 17:29:10 +0700

    membuat fungsi untuk menyetak daftar pasien yang ditangani dokter tertentu

--- asosiasi/asotiation.cpp
+++ asosiasi/asotiation.cpp
@@ -47,3 +47,11 @@ void dokter::tambahPasien(pasien* pPasien) {
     pPasien->tambahDokter(this);
 }
 
+void dokter::cetakPasien() {
+    cout << "Daftar Pasien dari dokter \"" << this->nama << "\":\n";
+    for (auto& a: daftar_pasien) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;                                   
+}
+
