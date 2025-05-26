746244ce3ae6b329d3991f4a9690fc72ccae68e3
zaknisdev <zakianisf1710@gmail.com>
2025-05-26 17:49:30 +0700

    memanggil anggota dari class pelajar untuk diinputkan data

--- inheritance/inheritance.cpp
+++ inheritance/inheritance.cpp
@@ -35,3 +35,11 @@ class pelajar : public orang {
     }
     
 };
+
+int main() {
+    pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10,90) << endl;
+
+    return 0;
+}
